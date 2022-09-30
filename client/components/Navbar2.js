54bb8122291f2f3b8a0e478cb520b3f1bb3fe0b1
Joshua Reyes <reyes.j15@gmail.com>
2022-09-30 01:05:30 -0400

    updated style

--- client/components/Navbar2.js
+++ client/components/Navbar2.js
@@ -1,9 +1,12 @@
 import React from "react";
-import LanguageIcon from '@mui/icons-material/Language';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import SettingsIcon from '@mui/icons-material/Settings';
+import LanguageIcon from "@mui/icons-material/Language";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { logout } from "../store";
 
-export default function Navbar2() {
+function Navbar2({ handleClick, isLoggedIn }) {
   return (
     <div className="topbar">
       <div className="topbarWrapper">
@@ -22,9 +25,49 @@ export default function Navbar2() {
           <div className="topbarIconContainer">
             <SettingsIcon />
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
+          <div className="dropdown">
+            <img
+              src="https://media-exp1.licdn.com/dms/image/C4D03AQHHZ5wWQ_5STg/profile-displayphoto-shrink_200_200/0/1639623814731?e=1669852800&v=beta&t=wWADDsSbBpZDgxy4Oev5XzJaa6x69jl5RqRHF4dNQsE"
+              alt=""
+              className="topAvatar"
+            />
+            <div class="dropdown-content">
+              {isLoggedIn ? (
+                <div className="login">
+                  {/* The navbar will show these links after you log in */}
+                  <Link to="/home">Home</Link><br></br>
+                  <a href="#" onClick={handleClick}>
+                    Logout
+                  </a>
+                </div>
+              ) : (
+                <div className="login">
+                  {/* The navbar will show these links before you log in */}
+
+                  <Link to="/login">Login</Link><br></br>
+                  <Link to="/signup">Sign Up</Link>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+const mapState = (state) => {
+  return {
+    isLoggedIn: !!state.auth.id,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    handleClick() {
+      dispatch(logout());
+    },
+  };
+};
+
+export default connect(mapState, mapDispatch)(Navbar2);
